54f74408c70b02df1f84fb6474660450ba656a00
Justin <jfinney416@gmail.com>
2023-03-06 19:53:07 -0500

    Inital completion Signup Page UI

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -1,76 +1,154 @@
+'use client';
 import styles from "./page.module.css"
 import Form from "./form"
-import Script from "next/script"
+import { useState, useEffect } from "react";
 
 export default function Page() {
-    const form = () => {
-        return (
-            <form action="/api/form" method="post" className={styles.signUp_Form_Container}>
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="text" id="first" name="first" placeholder=" " />
-                    <label htmlFor="first" className={styles.signUp_Form_Container_Item_Placeholder}>First Name</label>
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName: "",
+        userName: "",
+        houseName: "",
+        userEmail: "",
+        userPassword: ""
+    });
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
+    const [houseError, setHouseError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [userPasswordError, setUserPasswordError] = useState(false);
+    const [userRePasswordError, setUserRePasswordError] = useState(false);
+    const hasError = houseError || emailError || userPasswordError || userRePasswordError;
 
-                </div>
+    // Handles the Signup form changes and validation
+    const handleInputChange = (e) => {
+        setUser(prevState => ({ ...prevState, [e.target.name]: e.target.value }))
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="text" id="last" name="last" placeholder=" " />
-                    <label htmlFor="last" className={styles.signUp_Form_Container_Item_Placeholder}>Last Name</label>
-                </div>
+        let alphaNumericCheck = /^[a-z0-9]+$/i;
+        let emailValidation = /^(([^<>()[\]\\.,;: \s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="text" id="userName" name="userName" placeholder=" " />
-                    <label htmlFor="userName" className={styles.signUp_Form_Container_Item_Placeholder}>User Name</label>
-                </div>
+        if (e.target.name == "houseName" && !alphaNumericCheck.test(e.target.value) && !houseError) {
+            setHouseError(true);
+            document.getElementById('houseName')!.style.background = "red";
+        }
+        else if (e.target.name == "houseName" && alphaNumericCheck.test(e.target.value) && houseError) {
+            setHouseError(false);
+            document.getElementById('houseName')!.style.background = "white";
+        }
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="text" id="house" name="house" placeholder=" " required pattern="^[a-z0-9]+$" />
-                    <label htmlFor="house" className={styles.signUp_Form_Container_Item_Placeholder}> House Name</label>
-                </div>
+        if (e.target.name == "userEmail" && !(emailValidation.test(e.target.value))) {
+            setEmailError(true);
+            document.getElementById('userEmail')!.style.background = "red";
+        }
+        else if (e.target.name == "userEmail" && emailValidation.test(e.target.value)) {
+            setEmailError(false);
+            document.getElementById('userEmail')!.style.background = "white";
+        }
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="email" id="email" name="email" placeholder=" " required />
-                    <label htmlFor="email" className={styles.signUp_Form_Container_Item_Placeholder}>Email</label>
-                </div>
+        if (e.target.name == "userPassword" && (e.target.value.length < 8)) {
+            setUserPasswordError(true);
+            setPassword(e.target.value);
+            document.getElementById('userPassword')!.style.background = "red";
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="password" id="password" name="password" placeholder=" " required minLength={8}/>
-                    <label htmlFor="password" className={styles.signUp_Form_Container_Item_Placeholder}>Password</label>
-                </div>
+        }
+        else if (e.target.name == "userPassword" && (e.target.value.length >= 8)) {
+            setUserPasswordError(false);
+            setPassword(e.target.value);
+            document.getElementById('userPassword')!.style.background = "white";
+        }
 
-                <div className={styles.signUp_Form_Container_Item}>
-                    <div className={styles.signUp_Form_Container_Item_Cut}></div>
-                    <input type="password" id="repassword" name="repassword" placeholder=" " required minLength={8}/>
-                    <label htmlFor="repassword" className={styles.signUp_Form_Container_Item_Placeholder}>Re-enter pasword</label>
-                </div>
-
-                <div className={styles.signUp_Submit_Section}>
-                    <div className={styles.signUp_Submit_Section_buttons}>
-                        <button className={styles.signUp_Not_Quite_Display}>Not Quite</button>
-                        <button type="submit" className={styles.signUp_Submit_Section_SignUp_Button}>Sign-up</button>
-                    </div>
-                    <h2 className={styles.signUp_Error_Message_Display}>Error message:</h2>
-                </div>
-            </form>
-        )
-
+        if (e.target.name == "rePassword" && e.target.value === password) {
+            setUserRePasswordError(false);
+            setRePassword(e.target.value);
+            document.getElementById('rePassword')!.style.background = "white";
+        }
+        else if (e.target.name == "rePassword" && e.target.value !== password) {
+            setUserRePasswordError(true);
+            setRePassword(e.target.value);
+            document.getElementById('rePassword')!.style.background = "red";
+        }
 
     }
 
-    <script>
-        function ValidateFormWithJS() {
+    const form = () => {
+        return (
+            // Signup Form Ui
+            <form action="/api/form" method="post" className={styles.signUpFormContainer}>
 
-        }
-    </script>
+                <div className={styles.signUpFormContainerItem}>
 
+                    <input type="text" id="firstName" name="firstName" placeholder=" " onChange={handleInputChange} />
+                    <label htmlFor="firstName" className={styles.signUpFormContainerItemPlaceholder}>First Name</label>
+
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="text" id="lastName" name="lastName" placeholder=" " onChange={handleInputChange} />
+                    <label htmlFor="lastName" className={styles.signUpFormContainerItemPlaceholder}>Last Name</label>
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="text" id="userName" name="userName" placeholder=" " onChange={handleInputChange} />
+                    <label htmlFor="userName" className={styles.signUpFormContainerItemPlaceholder}>User Name</label>
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="text" id="houseName" name="houseName" placeholder=" " required pattern="^[a-z0-9]+$" onChange={handleInputChange} />
+                    <label htmlFor="houseName" className={styles.signUpFormContainerItemPlaceholder}> House Name</label>
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="email" id="userEmail" name="userEmail" placeholder=" " onChange={handleInputChange} required />
+                    <label htmlFor="userEmail" className={styles.signUpFormContainerItemPlaceholder}>Email</label>
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="password" id="userPassword" name="userPassword" placeholder=" " onChange={handleInputChange} required minLength={8} />
+                    <label htmlFor="userPassword" className={styles.signUpFormContainerItemPlaceholder}>Password</label>
+                </div>
+
+                <div className={styles.signUpFormContainerItem}>
+                    <input type="password" id="rePassword" name="rePassword" placeholder=" " onChange={handleInputChange} required minLength={8} />
+                    <label htmlFor="rePassword" className={styles.signUpFormContainerItemPlaceholder}>Re-Enter pasword</label>
+                </div>
+
+
+                {/* Error message display  */}
+                <div className={styles.signUpSubmitSection}>
+
+                    <div className={styles.signUpSubmitSectionbuttons}>
+
+                        {!hasError && (
+                            <button type="submit" className={styles.signUpSubmitSectionSignUpButton}>Sign-up</button>
+                        )}
+                        {hasError && (
+                            <button className={styles.signUpNotQuiteDisplay}>Not Quite</button>
+                        )}
+                    </div>
+                    <div className={styles.signUpErrorMessageDisplay} id="signUpErrorMessageId">
+                        {houseError && (
+                            <h2>Error: House names must contain only alpha numeric charectars</h2>
+                        )}
+                        {emailError && (
+                            <h2 >Error: Invaild Email</h2>
+                        )}
+                        {userPasswordError && (
+                            <h2 >Error: Passwords need to be atleast 8 characters long</h2>
+                        )}
+                        {userRePasswordError && (
+                            <h2 >Error: Passwords do not match</h2>
+                        )}
+                    </div>
+                </div>
+            </form>
+        )
+    }
+
+
+        // Returns Signup page container
     return (
-        <main className={styles.signUp_Page_Container}>
+        <main className={styles.signUpPageContainer}>
             <h1>Create an Account</h1>
             {form()}
 
